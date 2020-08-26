3855d86a382696066b8a3c06df73b51f698d640e
Fendi Ali <47903388+fendiali@users.noreply.github.com>
2020-08-26 16:25:20 +0700

    Create yt-tags-extractor.js

---
+++ yt-tags-extractor.js
@@ -0,0 +1,34 @@
+function fenTags() {
+	// Selecting the input element and get its value 
+	var url = document.getElementById("tags").value;
+	//clean url
+
+	var videoid = url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
+	if (videoid != null) {
+		var vid = videoid[1];
+		$.getJSON('https://www.googleapis.com/youtube/v3/videos?alt=json&part=snippet&id=' + vid + '&type=videos&key=AIzaSyA-dlBUjVQeuc4a6ZN4RkNUYDFddrVLxrA', function (data) {
+
+			html = '';
+
+			var cenel = "<b>Channel:</b> " + data.items[0].snippet.channelTitle + "<br/><b>Judul:</b> " + data.items[0].snippet.title;
+
+			$(".channel").append(cenel);
+			
+			html += "<h2>Tags</h2> <ul>";
+
+			$.each(data.items[0].snippet.tags, function (index, fenItems) {
+				html += "<li class='tags'>" + fenItems + "</li>";
+
+			});
+			html += "</ul>";
+			
+			$("#fen-results-msg").append(html);
+		
+			document.getElementById('form-tags').innerHTML = "";
+			
+		});
+	} else {
+		console.log("The youtube url is not valid.");
+	}
+
+}
